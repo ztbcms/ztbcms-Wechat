7d77babc64093a1f6018e1cb4cc2ce5bdc35cc91
zhlhuang <364626853@qq.com>
2019-08-16 15:45:45 +0800

    发送模板消息展示和发送模板消息测试

--- Controller/MiniController.class.php
+++ Controller/MiniController.class.php
@@ -10,10 +10,101 @@ namespace Wechat\Controller;
 
 
 use Common\Controller\AdminBase;
+use Wechat\Model\MiniTemplateListModel;
 use Wechat\Model\MiniUsersModel;
+use Wechat\Model\OfficesModel;
+use Wechat\Service\MiniTemplateService;
 
 class MiniController extends AdminBase
 {
+    /**
+     * 发送模板消息测试
+     *
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidArgumentException
+     * @throws \Think\Exception
+     */
+    function sendTemplateMsg()
+    {
+        $appId = I('post.app_id');
+        $touserOpenid = I('post.touser_openid');
+        $templateId = I('post.template_id');
+        $keywords = I('post.keywords');
+        $page = I('post.page');
+        $sendData = [];
+        foreach ($keywords as $keyword) {
+            $sendData[$keyword['key']] = $keyword['value'];
+        }
+        $templateService = new MiniTemplateService($appId);
+        $res = $templateService->sendTemplateMessage($touserOpenid, $templateId, $page, $sendData);
+        $this->ajaxReturn($res);
+    }
+
+    /**
+     * 删除消息模板
+     *
+     * @throws \Think\Exception
+     */
+    function deleteTemplate()
+    {
+        $id = I('post.id');
+        $miniTemplateModel = new MiniTemplateListModel();
+        $template = $miniTemplateModel->where(['id' => $id])->find();
+        if ($template) {
+            $res = $miniTemplateModel->where(['id' => $template['id']])->delete();
+            if ($res) {
+                $this->ajaxReturn(self::createReturn(true, [], '删除成功'));
+            } else {
+                $this->ajaxReturn(self::createReturn(false, [], '删除失败'));
+            }
+        } else {
+            $this->ajaxReturn(self::createReturn(false, [], '找不到模板消息记录'));
+        }
+    }
+
+    /**
+     * 同步消息模板
+     *
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
+     * @throws \Think\Exception
+     */
+    function syncTemplateList()
+    {
+        //获取所有的小程序
+        $miniOfiiceModel = new OfficesModel();
+        $minioffices = $miniOfiiceModel->where(['account_type' => OfficesModel::ACCOUNT_TYPE_MINI])->field("app_id")->select();
+        foreach ($minioffices as $minioffice) {
+            $appId = $minioffice['app_id'];
+            $templateService = new MiniTemplateService($appId);
+            $templateService->getTemplateList();
+        }
+        $this->ajaxReturn(self::createReturn(true, [], '同步成功'));
+    }
+
+    /**
+     * 展示消息模板列表
+     */
+    function templateList()
+    {
+        if (IS_AJAX) {
+            $appId = I('get.app_id', '');
+            $title = I('get.title', '');
+            $page = I('get.page', 1);
+            $limit = I('get.limit', 20);
+            $where = [];
+            if ($appId) {
+                $where['app_id'] = ['like', '%'.$appId.'%'];
+            }
+            if ($title) {
+                $where['title'] = ['like', '%'.$title.'%'];
+            }
+            $miniTemplateModel = new MiniTemplateListModel();
+            $res = $miniTemplateModel->where($where)->page($page, $limit)->order('id DESC')->select();
+            $totalCount = $miniTemplateModel->where($where)->count();
+            $this->ajaxReturn(self::createReturnList(true, $res ? $res : [], $page, $limit, $totalCount, ceil($totalCount / $limit), '获取成功'));
+        }
+        $this->display('templatelist');
+    }
+
     /**
      * 获取用户信息
      *
@@ -38,7 +129,7 @@ class MiniController extends AdminBase
                 $where['nick_name'] = ['like', '%'.$nickName.'%'];
             }
             $miniUsersModel = new MiniUsersModel();
-            $res = $miniUsersModel->where($where)->page($page, $limit)->select();
+            $res = $miniUsersModel->where($where)->page($page, $limit)->order('id DESC')->select();
             $totalCount = $miniUsersModel->where($where)->count();
             $this->ajaxReturn(self::createReturnList(true, $res ? $res : [], $page, $limit, $totalCount, ceil($totalCount / $limit), '获取成功'));
         }
